f4f63349595d9870bd33bb0f8939df5aa62f30cf
Vyacheslav Rzhevskiy <rzhevskiy.va@yandex.ru>
2018-03-31 19:05:33 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace DeserializeXML
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter path to directory.");
+                Console.WriteLine("Usage: DeserializeXML <path>");
+                return 1;
+            }
+
+            string directoryPath = args[0];
+            if (!Directory.Exists(directoryPath))
+            {
+                Console.WriteLine("Directory does not exists.");
+                return 1;
+            }
+
+            // Get files in directory(and subdirectoies) with extension .xml
+            var filesToDeserialize = Directory.GetFiles(directoryPath, "*.xml", SearchOption.AllDirectories);
+
+            ///TODO: deserialize each file
+            foreach (string file in filesToDeserialize)
+            {
+                ProcessFile(file);
+            }
+
+            return 0;
+        }
+
+        static void ProcessFile(string file)
+        {
+
+        }
+    }
+}
